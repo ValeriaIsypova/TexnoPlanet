c8cc8237609dc3854ba4d668453e01b18d7f7af7
ValeriaIsypova <isypovavaleria3@gmail.com>
2023-02-15 23:25:02 +0600

    добавление карточек товара

---
+++ resources/views/cart.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section class="cart">
+    <div class="container">
+        @if (Auth::user())
+        <h2 class="title">
+            Корзина
+        </h2>
+        <?php $sum = 0; ?>
+        <div class="cart-block">
+            <div class="cart-items col-12 col-md-9 gap-3 order-md-1 order-0">
+                @foreach ($products as $prod)
+
+                <div class="cart-item card col-12">
+                    <div class="cart-pic-box">
+                        <img src="/public/img/{{$prod -> photo}}" alt="" class="img-fliud">
+                    </div>
+                    <div class="text-box">
+                        <p class="card-title">{{$prod -> title}}</p>
+                        <div class="count-box">
+                            <a href="/public/cartrem/{{$prod -> id}}" class="add-rem">-</a>
+                            <p class="text count">{{$prod -> count}} шт</p>
+                            <a href="/public/cartadd/{{$prod -> id}}" class="add-rem">+</a>
+                        </div>
+                        <p class="text price">{{$prod -> price * $prod -> count}} руб.</p>
+                    </div>
+                    <a href="/public/cartall/{{$prod -> id}}" class="btn main-btn">Удалить</a>
+                </div>
+                <?php $sum += $prod -> price * $prod -> count; ?>
+                @endforeach
+            </div>
+            <div class="total-cost col-12 col-md-3 order-1 order-md-0">
+                <div class="total">
+                    <h2>Итого:</h2>
+                    <p class="text price">{{$sum}}</p>
+                </div>
+            </div>
+        </div>
+        @else
+        <h1>Вы не авторизованны, пожалуйста войдите в аккаунт</h1>
+        @endif
+
+    </div>
+</section>
+
+@endsection
