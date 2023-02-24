904ea08fde03ac133c5e19a00617048b347b37e5
ValeriaIsypova <isypovavaleria3@gmail.com>
2023-02-24 19:44:47 +0600

    new

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,45 +4,45 @@
 <div class="container">
 	<div class="row">
 		<div id="carouselExampleIndicators" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="/public/product_banner_1.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="/public/product_banner.png" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="/public/product_banner_3.png" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Предыдущий</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Следующий</span>
-  </button>
-</div>
-<div class="about">
-	<div class="about_title">
-		<h1>О нас</h1>
-	</div>
-	<div class="about_section col-12 d-flex">
-		<div class="txt_about col-6">
-			<h2>В нашем интернет-магазине компьютерной техники "Texno Planet" вы можете преобрести различные комплектующие для ПК, периферию, а так же различные модели ноутбуков и персональных компьютеров. Наш магазин радует клиентов уже более 10 лет!</h2>
-		</div>
-		<div class="logo_about col-6">
-			<img src="/public/logo.png">
-		</div>
-	</div>
-</div>
-	</div>
-</div>
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/public/product_banner_1.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="/public/product_banner.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="/public/product_banner_3.png" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Предыдущий</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Следующий</span>
+        </button>
+      </div>
+        <div class="about">
+          <div class="about_title">
+            <h1>О нас</h1>
+          </div>
+          <div class="about_section col-12 d-flex">
+            <div class="txt_about col-6">
+              <h2>В нашем интернет-магазине компьютерной техники "Texno Planet" вы можете преобрести различные комплектующие для ПК, периферию, а так же различные модели ноутбуков и персональных компьютеров. Наш магазин радует клиентов уже более 10 лет!</h2>
+            </div>
+            <div class="logo_about col-6">
+              <img src="/public/logo.png">
+            </div>
+          </div>
+        </div>
+          </div>
+        </div>
 
-@endsection
+        @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Вы зарегистрироаны!') }}
                 </div>
             </div>
         </div>
